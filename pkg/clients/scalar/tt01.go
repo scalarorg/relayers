aafbaf2a6f05cadcc2f317fdeb7571d93a3c7d33
Nhan Pham <dantedbartel@gmail.com>
2024-11-06 14:10:46 +0700

    Temporary comment out some conflicts

--- pkg/clients/scalar/tt01.go
+++ pkg/clients/scalar/tt01.go
@@ -1,34 +1,34 @@
 package scalar_clients
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	axelartypes "github.com/axelarnetwork/axelar-core/x/axelarnet/types"
-	"github.com/cosmos/cosmos-sdk/client"
-	"google.golang.org/grpc"
-)
+// 	axelartypes "github.com/axelarnetwork/axelar-core/x/axelarnet/types"
+// 	"github.com/cosmos/cosmos-sdk/client"
+// 	"google.golang.org/grpc"
+// )
 
-// QueryClient is similar to AxelarQueryClientType
-type AxelarQueryClient struct {
-	axelartypes.QueryClient
-	ctx context.Context
-}
+// // QueryClient is similar to AxelarQueryClientType
+// type AxelarQueryClient struct {
+// 	axelartypes.QueryClient
+// 	ctx context.Context
+// }
 
-// Create a new query client
-func NewAxelarQueryClient(grpcConn *grpc.ClientConn) *AxelarQueryClient {
-	return &AxelarQueryClient{
-		QueryClient: axelartypes.NewQueryClient(grpcConn),
-		ctx:         context.Background(),
-	}
-}
+// // Create a new query client
+// func NewAxelarQueryClient(grpcConn *grpc.ClientConn) *AxelarQueryClient {
+// 	return &AxelarQueryClient{
+// 		QueryClient: axelartypes.NewQueryClient(grpcConn),
+// 		ctx:         context.Background(),
+// 	}
+// }
 
-// For signing transactions (similar to AxelarSigningClient)
-type AxelarTxClient struct {
-	clientCtx client.Context
-}
+// // For signing transactions (similar to AxelarSigningClient)
+// type AxelarTxClient struct {
+// 	clientCtx client.Context
+// }
 
-func NewAxelarTxClient(clientCtx client.Context) *AxelarTxClient {
-	return &AxelarTxClient{
-		clientCtx: clientCtx,
-	}
-}
+// func NewAxelarTxClient(clientCtx client.Context) *AxelarTxClient {
+// 	return &AxelarTxClient{
+// 		clientCtx: clientCtx,
+// 	}
+// }
